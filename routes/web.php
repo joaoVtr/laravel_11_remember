8d4158b0628e58a20d8b8c5625869a23253c8caf
joaoVtr <joao_vitor_mj@hotmail.com>
2024-04-01 00:03:42 -0300

    Criação da tabela de notas, assim como seus dados

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\TesteController;
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/teste', [TesteController::class, 'teste'])->name('teste');
+
+Route::resource('note', NoteController::class);
+
